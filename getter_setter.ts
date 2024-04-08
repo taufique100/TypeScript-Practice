015e1c66e4c916f077d31fe9d08d1130855c9a50
Taufique-Speqto <taufique.ali@speqto.com>
2024-04-08 16:51:15 +0530

    getter and setter property added in practice code

---
+++ getter_setter.ts
@@ -0,0 +1,33 @@
+class User {
+    readonly city: string = "Jaipur"
+
+    private _courseCount = 1
+
+    constructor (
+        public email: string,
+        public name: string
+    ){
+
+    }
+
+    private deleteToken(){
+        console.log('Token Deleted');
+        
+    }
+    get AppleEmail():string{
+        return `apple ${this.email}`
+    }
+
+    get courseCount():number{
+        return this._courseCount
+    }
+    set courseCount(courseNum){
+        if(courseNum <= 1){
+            throw new Error("Course Count should be More Than 1")
+        }
+        this._courseCount = courseNum
+    }
+
+}
+
+const user = new User('tau@gamil.com', 'abcd')
